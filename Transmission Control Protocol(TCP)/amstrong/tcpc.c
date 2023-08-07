595abfdb6c653ef651ffeabadf107c3598179873
NIBIN <96453329+nibinpsreenivas@users.noreply.github.com>
2023-08-07 18:48:05 +0530

    Add files via upload

---
+++ Transmission Control Protocol(TCP)/amstrong/tcpc.c
@@ -0,0 +1,81 @@
+#include<sys/socket.h>
+#include<fcntl.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+   int k;
+   char buf[100];
+   int sock_desc,temp_sock_desc;
+   socklen_t len;
+   struct sockaddr_in server,client;
+   
+   sock_desc = socket(AF_INET,SOCK_STREAM,0);
+   
+   if(sock_desc==-1)
+   {
+     printf("\nerror in sock_desc");
+   }
+   
+   client.sin_family= AF_INET;
+   client.sin_addr.s_addr=INADDR_ANY;
+   client.sin_port = 3001;
+   
+ 
+   len=sizeof(client);
+   
+    k= connect(sock_desc,(struct sockaddr*) &client,sizeof(client));
+   
+    if(temp_sock_desc==-1)
+    {
+     printf("\nerror in temp_sock_desc");
+    }
+    while(1)
+    {
+       
+         printf("\nenter the number");
+         fgets(buf,100,stdin);
+         
+         int c= atoi(buf);
+         if(c==1111)
+         {
+           k=send(sock_desc,buf,100,0);
+            if(k==-1)
+            {
+              printf("\nerror in sending");
+            }
+           printf("\n Exiting.....");
+           exit(0);
+         }
+         k=send(sock_desc,buf,100,0);
+         if(k==-1)
+         {
+           printf("\nerror in sending");
+         }
+         
+         k=recv(sock_desc,buf,100,0);
+         if(k==-1)
+         {
+           printf("\nerror in recv");
+         }
+        
+         if(atoi(buf)==0)
+         {
+           printf("\nThe number is armstrong number");
+         }
+         else
+         {
+           printf("\nThe number is not armstrong number");
+         }
+       }   
+        
+   }
+   
+   
+   
+   
+   
---
+++ Transmission Control Protocol(TCP)/amstrong/tcps.c
@@ -0,0 +1,132 @@
+#include<sys/socket.h>
+#include<fcntl.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+   int k;
+   char buf[100];
+   int sock_desc,temp_sock_desc;
+   socklen_t len;
+   struct sockaddr_in server,client;
+   
+   sock_desc = socket(AF_INET,SOCK_STREAM,0);
+   
+   if(sock_desc==-1)
+   {
+     printf("\nerror in sock_desc");
+   }
+   
+   server.sin_family= AF_INET;
+   server.sin_addr.s_addr=INADDR_ANY;
+   server.sin_port = 3001;
+   
+   client.sin_family= AF_INET;
+   client.sin_addr.s_addr=INADDR_ANY;
+   client.sin_port = 3001;
+   
+   k=bind(sock_desc,(struct sockaddr*) &server,sizeof(server));
+   
+   
+   if(k==-1)
+   {
+     printf("\nerror in bind");
+   }
+   
+   k=listen(sock_desc,5);
+   
+   if(k==-1)
+   {
+     printf("\nerror in listen");
+   }
+   len=sizeof(client);
+   
+   temp_sock_desc = accept(sock_desc,(struct sockaddr*) &client,&len);
+   
+    if(temp_sock_desc==-1)
+    {
+     printf("\nerror in temp_sock_desc");
+    }
+    while(1)
+    {
+         k=recv(temp_sock_desc,buf,100,0);
+         if(k==-1)
+         {
+           printf("\nerror in recv");
+         }
+        
+         int c= atoi(buf);
+      
+         if(c==1111)
+         {
+           printf("\n Exiting.....");
+           exit(0);
+         }
+       int count=0,temp;
+       temp=c;
+      while(temp!=0)
+      {
+        temp=temp/10;
+        count++;
+      }
+      temp=c;
+      int sum=0,p,d;
+      int flag;
+      while(temp!=0)
+      {
+        d=temp%10;
+        p=1;
+        for(int i=0;i<count;i++)
+        {
+          p=p*d;
+        }
+        temp=temp/10;
+        sum=sum+p;
+       }
+       
+      
+     
+      if(sum==c)
+      {
+        flag=0;
+      }      
+      else
+      {
+        flag=1;
+      }
+         printf("\nThe number from client is %s",buf);
+         sprintf(buf,"%d",flag);
+         k=send(temp_sock_desc,buf,100,0);
+         if(k==-1)
+         {
+           printf("\nerror in sending");
+         }
+       }   
+        
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
